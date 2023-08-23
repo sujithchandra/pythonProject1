904d0ef2fc732e916f4f60cbcdcbd7b530daba0b
sujithchandra <sakamsujithchandra@gmail.com>
2023-08-23 16:12:01 +0530

    Deleted

--- ApiProject/Api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Student
-
-
-class Studentserializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = ('__all__')  #('student_name', 'student_school', 'student_course')
